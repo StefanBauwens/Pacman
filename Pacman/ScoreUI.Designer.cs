e37ae999de6128072f6bdbb6b6ac6c11bc0c382e
cindyho97 <cindy.ho8197@gmail.com>
2016-10-14 16:39:37 +0200

    modified classes

--- Pacman/ScoreUI.Designer.cs
+++ Pacman/ScoreUI.Designer.cs
@@ -28,10 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.scoreTextLabel = new System.Windows.Forms.Label();
+            this.scoreValueLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // scoreTextLabel
+            // 
+            this.scoreTextLabel.AutoSize = true;
+            this.scoreTextLabel.Location = new System.Drawing.Point(31, 22);
+            this.scoreTextLabel.Name = "scoreTextLabel";
+            this.scoreTextLabel.Size = new System.Drawing.Size(49, 17);
+            this.scoreTextLabel.TabIndex = 0;
+            this.scoreTextLabel.Text = "Score:";
+            this.scoreTextLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // scoreValueLabel
+            // 
+            this.scoreValueLabel.AutoSize = true;
+            this.scoreValueLabel.Location = new System.Drawing.Point(90, 21);
+            this.scoreValueLabel.Name = "scoreValueLabel";
+            this.scoreValueLabel.Size = new System.Drawing.Size(40, 17);
+            this.scoreValueLabel.TabIndex = 1;
+            this.scoreValueLabel.Text = "1000";
+            // 
+            // ScoreUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.scoreValueLabel);
+            this.Controls.Add(this.scoreTextLabel);
+            this.Name = "ScoreUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label scoreTextLabel;
+        private System.Windows.Forms.Label scoreValueLabel;
     }
 }
