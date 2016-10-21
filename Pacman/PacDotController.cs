27af9b435189264445041f37bc036f3bbd00eadc
StefanBauwens <stefan.bauwens@hotmail.com>
2016-10-21 15:57:38 +0200

    removed some lines in pacdots class

--- Pacman/PacDotController.cs
+++ Pacman/PacDotController.cs
@@ -11,29 +11,5 @@ namespace Pacman
         private PacDotModel pacDotModel;
         private ScoreUI scoreUI;
 
-        public ScoreController()
-        {
-            this.scoreModel = new ScoreModel();
-            this.scoreUI = new ScoreUI();
-        }
-
-        // returns view
-        public ScoreUI view
-        {
-            get
-            {
-                return this.scoreUI;
-            }
-        }
-
-        // executes when the observable is changed
-        public void notify(int newNrDots)
-        {
-            // update model with new value
-            this.scoreModel.score = newNrDots;
-
-            // update view with new value
-            this.scoreUI.updateScore(newNrDots);
-        }
     }
 }
