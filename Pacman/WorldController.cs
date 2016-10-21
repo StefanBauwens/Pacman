6f4b0cecbaae591d074a7384d67280d8b7f4133d
StefanBauwens <stefan.bauwens@hotmail.com>
2016-10-21 17:00:37 +0200

    working on worldmap

---
+++ Pacman/WorldController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    public class WorldController
+    {
+        WorldModel model;
+        WorldUI worldUI;
+
+        public WorldController()
+        {
+            model = new WorldModel();
+            worldUI = new WorldUI(this);
+
+            //draw the world
+            for (int rows = 0; rows < model.Map2D.GetLength(0); rows++)
+            {
+                for (int colls = 0; colls < model.Map2D.GetLength(1); colls++)
+                {
+
+                }
+            }
+        }
+
+        public WorldUI view
+        {
+            get { return worldUI; }
+        }
+    }
+}
